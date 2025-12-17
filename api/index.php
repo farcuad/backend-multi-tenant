9eab020265a8c3e2288398977d41102686f623b6
Andres Calles <meridaalejandro81@gmail.com>
2025-12-16 22:14:00 -0400

    Deploy del backend

--- api/index.php
+++ api/index.php
@@ -1,4 +1,11 @@
 <?php 
-// Reenvía las peticiones de Vercel al index.php normal de Laravel
+// Definir la ruta de storage en /tmp (Serverless)
+$_SERVER['SCRIPT_FILENAME'] = __DIR__ . '/../public/index.php';
+$_SERVER['SCRIPT_NAME'] = '/index.php';
+
+// Sobreescribe la variable de entorno de Laravel para que use /tmp
+putenv('LARAVEL_STORAGE_PATH=/tmp/storage');
+
+// Ejecuta la aplicación
 require __DIR__ . '/../public/index.php';
 ?>
