fe5e38817aede7347879c24dd7800b1886930c47
Andres Calles <meridaalejandro81@gmail.com>
2026-01-09 11:48:17 -0400

    Endpoint de low stock arreglado

--- app/Http/Controllers/Api/LowStockController.php
+++ app/Http/Controllers/Api/LowStockController.php
@@ -5,11 +5,16 @@ namespace App\Http\Controllers\Api;
 use App\Models\Product;
 use App\Http\Resources\LowStockAlertResource;
 use App\Http\Controllers\Controller;
+
 class LowStockController extends Controller
 {
     public function index()
     {
-        $lowStockProducts = Product::whereColumn('stock', '<=', 'min_stock')->orderBy('stock', 'asc')->get();
+        $lowStockProducts = Product::whereNotNull('min_stock')
+            ->whereColumn('stock', '<=', 'min_stock')
+            ->orderBy('stock', 'asc')
+            ->get();
+
         return LowStockAlertResource::collection($lowStockProducts);
     }
 }
